cf1d2893acdb5532b2710e748fedcd7212282f70
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-02-24 21:00:21 +0000

    fix: use specific branch to fix independent rx dual tuner issue.

--- install_gr_sdrplay3.sh
+++ install_gr_sdrplay3.sh
@@ -1,5 +1,7 @@
 #clone the repo
-git clone https://github.com/fventuri/gr-sdrplay3.git
+# git clone https://github.com/fventuri/gr-sdrplay3.git
+# clone the branch containing the hot fix for issue https://github.com/fventuri/gr-sdrplay3/issues/31
+git clone -b 3.9-fix_tuner2_frequency_for_independent_rx https://github.com/fventuri/gr-sdrplay3.git
 #cd into the cloned repo
 cd gr-sdrplay3
 #built the OOT module
