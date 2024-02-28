2f2ef242342904c2af08049850c665a70df42a58
Jimmy Fitzpatrick <jcfitzpatrick12@gmail.com>
2024-02-28 18:43:54 +0000

    reverted back to cloning from main following succesful fix with fventuri

--- install_gr_sdrplay3.sh
+++ install_gr_sdrplay3.sh
@@ -1,7 +1,8 @@
 #clone the repo
-# git clone https://github.com/fventuri/gr-sdrplay3.git
+git clone https://github.com/fventuri/gr-sdrplay3.git
+# git clone https://github.com/jcfitzpatrick12/gr-sdrplay3.git
 # clone the branch containing the hot fix for issue https://github.com/fventuri/gr-sdrplay3/issues/31
-git clone -b 3.9-fix_tuner2_frequency_for_independent_rx https://github.com/fventuri/gr-sdrplay3.git
+# git clone -b 3.9-fix_tuner2_frequency_for_independent_rx https://github.com/fventuri/gr-sdrplay3.git
 #cd into the cloned repo
 cd gr-sdrplay3
 #built the OOT module
